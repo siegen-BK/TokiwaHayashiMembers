03b535c33bf5f48ffb12c634e9684dc051768b08
siegen <249540061+siegen-BK@users.noreply.github.com>
2026-02-10 19:28:39 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,23 @@
+const CACHE = 'hayashi-app-v1';
+const ASSETS = [
+  './','./index.html','./styles.css','./config.js','./config-loader.js',
+  './router.js','./renderers.js','./app.js','./manifest.json',
+  './icons/icon-192.png','./icons/icon-512.png'
+];
 
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+});
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  const isAsset = ASSETS.some(p=>url.pathname.endsWith(p.replace('./','/')));
+  e.respondWith(
+    (isAsset
+      ? caches.match(e.request).then(r=>r||fetch(e.request))
+      : fetch(e.request).catch(()=>caches.match(e.request))
+    )
+  );
+});
