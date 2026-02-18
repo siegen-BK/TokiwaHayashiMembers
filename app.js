7b4331f972874511c3c0305d13a67f0543c87820
siegen <249540061+siegen-BK@users.noreply.github.com>
2026-02-18 15:32:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -445,9 +445,7 @@
 
     // click（追加/削除/スワップ/配置）
     $('#view').addEventListener('click', (e) => {
-      const t = e.target;
-      const t = (e.target && e.target.nodeType === 3) ? e.target.parentElement : e.target;
-
+const t = (e.target && e.target.nodeType === 3) ? e.target.parentElement : e.target;
 
       // 配置：区間/備考セル選択
       const cell = t.closest('#rows .cell[contenteditable="true"]');
